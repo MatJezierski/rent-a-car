d4e6c8609d257725964da646a910ecb929f75f6b
Mateusz Jezierski <mateuszjezierski@wp.eu>
2019-03-23 17:01:48 +0100

    [add]css styles and view layer

--- src/main/java/matjez/rentacarapp/models/Customer.java
+++ src/main/java/matjez/rentacarapp/models/Customer.java
@@ -5,8 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
@@ -16,10 +15,20 @@ import javax.persistence.Table;
 @Table(name="customer")
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "surname")
     private String surname;
+
+    @Column(name = "age")
     private int age;
+
+    @Column(name = "address")
     private String address;
 
 }
